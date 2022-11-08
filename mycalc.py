f7b39a0753ace664cf6c7ee3cf75b24205ecf3df
Seyoung <hsy6408@naver.com>
2022-11-08 11:12:01 +0900

    PC_Commit2

--- mycalc.py
+++ mycalc.py
@@ -11,6 +11,9 @@ def mul_func(n1, n2):
 def div_func(n1, n2):
     return n1 / n2
 
+def square_func(n1):
+    return n1 ** 2
+
 ## 전역
 num1, num2, result = 100, 200, 0
 
@@ -26,5 +29,8 @@ print(num1, '*', num2, '=', result)
 
 result = div_func(num1, num2)
 print(num1, '/', num2, '=', result)
+
+result = sqaure_func(num1)
+print(num1, '**', '2', '=', result)
 
 
